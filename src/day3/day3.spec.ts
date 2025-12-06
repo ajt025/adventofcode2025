e1d364cc42da35ea7b5baf4c848576ebb586303f
anthonyjtran <ajt025@ucsd.edu>
2025-12-06 16:32:23 -0600

    Solved Day 3.2

--- src/day3/day3.spec.ts
+++ src/day3/day3.spec.ts
@@ -1,12 +1,16 @@
 import day3 from './index';
 
 describe('On Day 3', () =>{
-    let input1 =
+    let input =
         "987654321111111\n" +
         "811111111111119\n" +
         "234234234234278\n" +
         "818181911112111\n";
     it(`part1`, ()=>{
-        expect(day3.solveForPartOne(input1)).toBe('357');
+        expect(day3.solveForPartOne(input)).toBe('357');
+    });
+
+    it(`part2`, ()=>{
+        expect(day3.solveForPartTwo(input)).toBe('3121910778619');
     })
 });
--- src/day3/index.ts
+++ src/day3/index.ts
@@ -3,7 +3,7 @@ import {parseLines} from "../util/parser";
 
 class Day3 extends Day {
 
-    constructor(){
+    constructor() {
         super(3);
     }
 
@@ -12,8 +12,8 @@ class Day3 extends Day {
 
         // sum each line's highest joltage
         let answer = lines.map(x => x.split("").map(Number))
-             .map(this.findHighestJoltage)
-             .reduce((acc, curr) => acc + curr, 0);
+            .map(this.findHighestJoltage)
+            .reduce((acc, curr) => acc + curr, 0);
 
         return String(answer);
     }
@@ -24,13 +24,39 @@ class Day3 extends Day {
         let highestTensIndex = input.indexOf(Math.max(...input.slice(0, input.length - 1)));
 
         // 2nd: find highest number that's after 1st pass digit
-        let highestOnesIndex = input.indexOf(Math.max(...input.slice(highestTensIndex + 1, input.length)));
+        let highestOnesIndex = input.indexOf(Math.max(...input.slice(highestTensIndex + 1, input.length)), highestTensIndex + 1);
 
         return input[highestTensIndex] * 10 + input[highestOnesIndex];
     }
 
     solveForPartTwo(input: string): string {
-        return input;
+        const lines = parseLines(input);
+
+        // sum each line's highest joltage
+        let answer = lines.map(x => x.split("").map(Number))
+            .map(this.findHighestJoltagePrime)
+            .reduce((acc, curr) => acc + curr, 0);
+
+        return String(answer);
+    }
+
+    findHighestJoltagePrime(input: number[]): number {
+        // similar approach to p1, but...
+            // instead of input.length - 1; slice end should be input.length - remainingBatteriesNeeded
+            // ensure we're indexOf()-ing after the last digit
+            // need to loop this 12 times
+
+        const remainingBatteriesNeeded = 12;
+        let lastDigitIndex = -1;
+        let result = '';
+        for (let i = 0; i < remainingBatteriesNeeded; ++i) {
+            lastDigitIndex = input.indexOf(
+                Math.max(...input.slice(lastDigitIndex + 1, input.length + 1 + i - remainingBatteriesNeeded)),
+                lastDigitIndex + 1);
+            result += String(input[lastDigitIndex]);
+        }
+
+        return Number(result);
     }
 }
 
